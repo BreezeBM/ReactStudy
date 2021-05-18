eca48c6aa69ae81483a926d2741aeb7021ac0eda
BreezeBm <seyya77@gmail.com>
2021-05-18 12:50:44 +0900

    useRef

--- live/src/App.js
+++ live/src/App.js
@@ -1,26 +1,54 @@
-import React, { useCallback, useState } from "react";
+import React, { useState, useContext, createContext, useRef } from "react";
 
 export default function App() {
-  const [text, setText] = useState(INITAL_TEXT);
-  const [showText, setShowText] = useState(true);
+  const boxListRef = useRef({});
+  // const boxListRef = useRef(123);
+  // boxListRef.current = 1
 
-  const setInitalText = useCallback((ref) => {
-    ref && setText(INITAL_TEXT);
-  }, []);
+  function onClick() {
+    let maxRight = 0;
+    let maxId = "";
+    for (const box of BOX_LIST) {
+      const ref = boxListRef.current[box.id];
+      if (ref) {
+        const rect = ref.getBoundingClientRect();
+        if (maxRight < rect.right) {
+          maxRight = rect.right;
+          maxId = box.id;
+        }
+      }
+    }
+    alert(`오른쪽 끝 요소는 ${maxId}입니다.`);
+  }
 
   return (
     <div>
-      {showText && (
-        <input
-          type="text"
-          ref={setInitalText}
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-      )}
-      <button onClick={() => setShowText(!showText)}>보이기 / 가리기</button>
+      <div style={{ display: "flex", flexWrap: "wrap", width: "100vw", height: "100%" }}>
+        {BOX_LIST.map((item) => (
+          <div
+            key={item.id}
+            ref={(ref) => (boxListRef.current[item.id] = ref)}
+            style={{
+              flex: "0 0 auto",
+              width: item.width,
+              height: 100,
+              backgroundColor: "yellow",
+              border: "solid 1px red",
+            }}
+          >{`box_${item.id}`}</div>
+        ))}
+      </div>
+      <button onClick={onClick}>오른쪽 끝 요소는?</button>
     </div>
   );
 }
 
-const INITAL_TEXT = "안녕하세요";
+const BOX_LIST = [
+  { id: 1, width: 170 },
+  { id: 2, width: 270 },
+  { id: 3, width: 180 },
+  { id: 4, width: 200 },
+  { id: 5, width: 190 },
+  { id: 6, width: 130 },
+  { id: 7, width: 220 },
+];
